f212591b263ce1d8391c19e86cb189c947108aba
Felipe Campos Clarke <felipecamposclarke@gmail.com>
2015-10-11 00:48:36 -0300

    Insert real array of tags in a challenge collection

--- server/methods/challenges.js
+++ server/methods/challenges.js
@@ -1,13 +1,26 @@
 Meteor.methods({
   challengeInsert: function(challengeAttributes) {
+
+    var tags = challengeAttributes.tags;
+    challengeAttributes = _.omit(challengeAttributes, "tags");
+
+    // validations single attributes
     check(challengeAttributes, {
       title: String,
       description: String
     });
 
+    // validate tags array
+    var validTags = _.all(tags, function(tag){
+      return _.include(challengeTags, tag);
+    })
+    if(!validTags)
+      throw new Meteor.Error('invalid-challenge', "You must set a valid array for you challenge");
+
     var user = Meteor.user();
     var challenge = _.extend(challengeAttributes, {
       userId: user._id,
+      tags: tags
     });
 
     var challengeId = Challenge.insert(challenge);
